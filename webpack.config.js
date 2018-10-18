424fd28addec9c8bbd0e01ef0f59d5f6bc6a345b
iw <igpfpyu@vip.qq.com>
2018-10-18 22:55:46 +0800

    js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,8 @@
-console.log('ac');
+const path=require('path');
+module.exports={
+    entry:path.resolve(__dirname, "src/index.js"),
+    output:{
+        path: path.resolve(__dirname, 'dist/'),
+        filename:"[name].js"
+    }
+}
