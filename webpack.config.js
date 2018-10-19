c87b5febd04f0335bebe4ddcb28fcca1855044a0
iw <igpfpyu@vip.qq.com>
2018-10-19 16:46:43 +0800

    f

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports={
     output:{
         path: path.resolve(__dirname, 'dist/'),
         filename:"[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(js|jsx)$/,
+                exclude:path.resolve(__dirname, "node_module"),
+                loader:"",
+                options:{
+                    presets:['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
     }
 };
