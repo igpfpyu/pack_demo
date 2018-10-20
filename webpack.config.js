4339f33e760d653c331ae5f5c27d1bc52e2dc4a2
iw <igpfpyu@vip.qq.com>
2018-10-20 18:19:55 +0800

    f

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path=require('path');
+const devServer=require('webpack-dev-server');
 module.exports={
     entry:path.resolve(__dirname, "src/index.js"),
     output:{
@@ -10,11 +11,33 @@ module.exports={
             {
                 test:/\.(js|jsx)$/,
                 exclude:path.resolve(__dirname, "node_module"),
-                loader:"",
+                loader:"babel-loader",
                 options:{
                     presets:['@babel/preset-env', '@babel/preset-react']
                 }
+            },{
+                test:/\.css$/,
+                use:["style-loader", "css-loader"]
+            },{
+                test:/\.(jpg|png)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit:8192
+                        }
+                    }
+                ]
+            },{
+                test:/\.svg$/,
+                loader:"svg-sprite-loader"
             }
         ]
+    },
+    devServer: {
+        contentBase:path.resolve(__dirname, 'public'),
+        compress:true,
+        port:9000,
+        hot:true,
     }
 };
