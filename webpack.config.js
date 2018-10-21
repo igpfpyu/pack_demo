5ca9e5902d97f8c831c4219bc12a6cb71b0fe347
iw <igpfpyu@vip.qq.com>
2018-10-21 14:55:44 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 const path=require('path');
-const devServer=require('webpack-dev-server');
+const htmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
-    entry:path.resolve(__dirname, "src/index.js"),
+    devtool:'inline-source-map',
+    mode:'development',
+    entry:{
+        index:path.resolve(__dirname, "src/index.js")
+    },
     output:{
         path: path.resolve(__dirname, 'dist/'),
-        filename:"[name].js"
+        filename:"[name].bundle.js",
+        publicPath: "/"
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'node_modules')],
+        extensions: ['*', '.jsx', '.js', '.json'],
     },
     module: {
         rules: [
             {
-                test:/\.(js|jsx)$/,
+                test:/\.(js|jsx)$/i,
                 exclude:path.resolve(__dirname, "node_module"),
                 loader:"babel-loader",
                 options:{
                     presets:['@babel/preset-env', '@babel/preset-react']
                 }
             },{
-                test:/\.css$/,
+                test:/\.css$/i,
                 use:["style-loader", "css-loader"]
             },{
-                test:/\.(jpg|png|svg)$/,
+                test:/\.(jpg|png|svg)$/i,
+                include: path.resolve(__dirname, 'src'),
                 use:[
                     {
                         loader:'url-loader',
@@ -29,15 +39,35 @@ module.exports={
                     }
                 ]
             },{
-                test:/\.svg$/,
+                test:/\.svg$/i,
+                include: path.resolve(__dirname, 'src/svg'),
                 loader:"svg-sprite-loader"
             }
         ]
     },
+    plugins: [
+        // new htmlWebpackPlugin({
+        //     template: '/',
+        //     filename:"index.html",
+        //     chunks: ['index'],
+        //     hash:true,
+        //     inject:true
+        // })
+        new htmlWebpackPlugin({
+            template:path.resolve(__dirname, 'index.html'),
+            title:"Deve",
+            inject:true,
+            filename:"index.html",
+            chunks: ['index'],
+            hot:true
+        })
+
+    ],
     devServer: {
-        contentBase:path.resolve(__dirname, 'public'),
-        compress:true,
-        port:9000,
-        hot:true,
+        contentBase:'/',
+        // hot: true,
+        inline:true,
+        compress: true,
+        port:9001
     }
 };
