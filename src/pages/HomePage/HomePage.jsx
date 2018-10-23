dd33083c399dc43d8077b0a41856bc2e237d0bfe
iw <igpfpyu@vip.qq.com>
2018-10-23 20:51:28 +0800

    js

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import  React, {Component} from 'react';
+import {
+    Button
+} from 'antd';
+export default class HomePage extends Component {
+   render(){
+       return (
+           <div>
+               <Button type="primary">primary</Button>
+           </div>
+       );
+   }
+}
