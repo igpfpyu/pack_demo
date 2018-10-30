214614184b740425cae5279d33254f50e187ef4c
iw <igpfpyu@vip.qq.com>
2018-10-30 16:55:16 +0800

    js

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ const MinCssExtract=require('mini-css-extract-plugin');
 const compressionPlugin=require('compression-webpack-plugin');
 const browserVersion= ['last 5 versions'];
 module.exports={
-    devtool:'inline-source-map',
-    mode:'development',
+    devtool:'eval-source-map',//开发 eval-source-map   生产 source-map;
+    // mode:'development',
     entry:{
         index:path.resolve(__dirname, "src/index.js"),
         vendor:['react', 'react-dom', 'antd']
@@ -84,22 +84,28 @@ module.exports={
         minimize:false,         //使用UglifyjsWebpackPlugin最小化捆绑包
         noEmitOnErrors: true,//在编译时出现错误时，使用跳过发射阶段。这可确保不会发出错误资产
         runtimeChunk: {         //增加了一个额外的块仅包含运行时每个入口点。此设置是以下别名：
-            name:module=>`abc~${module.name}`
+            name:"manifest/manifest",//module=>`abc~${module.name}`
         },
         splitChunks:{
-            chunks:chunk=> chunk.name==='vendor',//这表示将选择哪些块进行优化，有效值为all，async和initial。或者提供更多控制功能。返回值将指示是否包括每个块
-            minSize: 30000,     //要生成的块的最小大小（以字节为单位）
-            minChunks: 1,       //分割前必须共享模块的最小块数
-            maxAsyncRequests: 5,            //按需加载时的最大并行请求数
-            maxInitialRequests: 3,          //入口点处的最大并行请求数
-            name:module=>module.name,       //拆分块的名称。提供true将基于块和缓存组密钥自动生成名称
+            chunks:"initial",//这表示将选择哪些块进行优化，有效值为all，async和initial。或者提供更多控制功能。返回值将指示是否包括每个块chunk=> chunk.name==='vendor'?
+            // minSize: 30000,     //要生成的块的最小大小（以字节为单位）
+            // minChunks: 1,       //分割前必须共享模块的最小块数
+            // maxAsyncRequests: 5,            //按需加载时的最大并行请求数
+            // maxInitialRequests: 3,          //入口点处的最大并行请求数
+            name:true,                      //拆分块的名称。提供true将基于块和缓存组密钥自动生成名称module=>module.name
+            // filename:'newVendor',
             cacheGroups: {
+                // default:{
+                //     minChunks:2,
+                //     priority:-20,
+                //     reuseExistingChunk:true
+                // },
                 vendor:{
-                    name:'vendor',
-                    chunks: 'initial',
-                    priority: -10,
-                    reuseExistingChunk:true,
-                    test: /node_modules\/(.*)\.js/
+                    test: /node_modules\//,
+                    name:'lib/vendor',
+                    // chunks:'async',
+                    priority:-10,
+                    // reuseExistingChunk:true     //已经存在的块，即如果满足条件的块已经存在就使用已有的，不再创建一个新的块
                 }
             }
         }
@@ -115,7 +121,7 @@ module.exports={
             hot:true,
             chunksSortMode:"none"
         }),
-        new compressionPlugin(),
+        // new compressionPlugin(),
         new webpack.LoaderOptionsPlugin({
             options: {
                 postcss: function () {
